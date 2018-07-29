958a5e70e2bb4f39804a95cd6f6e04bbc80f0cb9
Peter Schmalfeldt <manifestinteractive@users.noreply.github.com>
2018-07-29 03:33:17 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -16,6 +16,7 @@ module.exports = NodeHelper.create({
 			var lastGesture = '';
 			var handPresent = false;
 			var timer = null;
+			var handStartPosition = [];
 
 			Cylon.robot({
 				connections: {
@@ -27,22 +28,7 @@ module.exports = NodeHelper.create({
 				},
 
 				work: function(device) {
-					device.leapmotion.on('hand', function(payload) {
-						if (!handPresent) {
-							handPresent = true;
-							lastGesture = 'hand_present';
-							self.sendSocketNotification('LEAP_MOTION_GESTURE', 'LEAP_MOTION_HAND_DETECTED');
-						}
-
-						clearTimeout(timer);
-						timer = setTimeout(function(){
-							handPresent = false;
-							lastGesture = 'hand_gone';
-							self.sendSocketNotification('LEAP_MOTION_GESTURE', 'LEAP_MOTION_HAND_MISSING');
-						}, 1000);
-					});
-
-					device.leapmotion.on('gesture', function(gesture) {
+					device.leapmotion.on('hand', function(hand) {
 						var direction = 'no motion';
 
 						var correctDirection = function (direction) {
@@ -68,40 +54,50 @@ module.exports = NodeHelper.create({
 							return orientation[config.orientation][direction];
 						}
 
-						if (gesture.type === 'swipe') {
-							var xdiff = (gesture.startPosition[0] - gesture.position[0]);
-							var ydiff = (gesture.startPosition[1] - gesture.position[1]);
-							var zdiff = (gesture.startPosition[2] - gesture.position[2]);
-
-							var movements = [
-								{
-									distance: Math.abs(xdiff),
-									coordinate: 'x',
-									type: (xdiff > 0) ? correctDirection('left') : correctDirection('right')
-								},
-								{
-									distance: Math.abs(ydiff),
-									coordinate: 'y',
-									type: (ydiff > 0) ? correctDirection('down') : correctDirection('up')
-								},
-								{
-									distance : Math.abs(zdiff),
-									coordinate : 'z',
-									type : (zdiff > 0) ? correctDirection('forward') : correctDirection('back')
-								}
-							];
-
-							// sort movements based on furthest distance
-							movements.sort(function(a, b) {
-								return b['distance'] - a['distance'];
-							});
-
-							// return the movement type with the greatest change in distance
-							direction = movements[0].type;
-
-						} else if (gesture.type === 'circle') {
-							direction = (gesture.normal[2] > 0) ? 'counter-clockwise' : 'clockwise';
+						if (!handPresent) {
+							handPresent = true;
+							lastGesture = 'hand_present';
+							handStartPosition = hand.palmPosition;
+							self.sendSocketNotification('LEAP_MOTION_GESTURE', 'LEAP_MOTION_HAND_DETECTED');
 						}
+
+						clearTimeout(timer);
+						timer = setTimeout(function(){
+							handPresent = false;
+							lastGesture = 'hand_gone';
+							handStartPosition = [];
+							self.sendSocketNotification('LEAP_MOTION_GESTURE', 'LEAP_MOTION_HAND_MISSING');
+						}, 1000);
+
+						var xdiff = (hand.palmPosition[0] - handStartPosition[0]);
+						var ydiff = (hand.palmPosition[1] - handStartPosition[1]);
+						var zdiff = (hand.palmPosition[2] - handStartPosition[2]);
+
+						var movements = [
+							{
+								distance: Math.abs(xdiff),
+								coordinate: 'x',
+								type: (xdiff > 0) ? correctDirection('left') : correctDirection('right')
+							},
+							{
+								distance: Math.abs(ydiff),
+								coordinate: 'y',
+								type: (ydiff > 0) ? correctDirection('down') : correctDirection('up')
+							},
+							{
+								distance : Math.abs(zdiff),
+								coordinate : 'z',
+								type : (zdiff > 0) ? correctDirection('forward') : correctDirection('back')
+							}
+						];
+
+						// sort movements based on furthest distance
+						movements.sort(function(a, b) {
+							return b['distance'] - a['distance'];
+						});
+
+						// return the movement type with the greatest change in distance
+						direction = movements[0].type;
 
 						if (lastGesture !== direction) {
 							if (config.watchGestureUp && direction === 'up') {
@@ -121,12 +117,6 @@ module.exports = NodeHelper.create({
 								lastGesture = direction;
 							} else if (config.watchGestureBack && direction === 'back') {
 								self.sendSocketNotification('LEAP_MOTION_GESTURE', 'LEAP_MOTION_SWIPE_BACK');
-								lastGesture = direction;
-							} else if (config.watchGestureClockwise && direction === 'clockwise') {
-								self.sendSocketNotification('LEAP_MOTION_GESTURE', 'LEAP_MOTION_GESTURE_CLOCKWISE');
-								lastGesture = direction;
-							} else if (config.watchGestureCounterClockwise && direction === 'counter-clockwise') {
-								self.sendSocketNotification('LEAP_MOTION_GESTURE', 'LEAP_MOTION_GESTURE_COUNTER_CLOCKWISE');
 								lastGesture = direction;
 							}
 						}
